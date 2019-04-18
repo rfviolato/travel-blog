b28fe54031b3ca79bd6f928250ece6f8ad5af76c
rfviolato <rfviolato@gmail.com>
2019-04-18 20:50:41 +0200

    refactor: Replace "Index" page with "Home" page

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { IndexPageTemplate } from '../../templates/index-page'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { HomePageTemplate } from '../../templates/home-page';
 
 const IndexPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
-  
+  const data = entry.getIn(['data']).toJS();
+
   if (data) {
     return (
-      <IndexPageTemplate
+      <HomePageTemplate
         image={data.image}
         title={data.title}
         heading={data.heading}
@@ -16,17 +16,17 @@ const IndexPagePreview = ({ entry, getAsset }) => {
         intro={data.intro || { blurbs: [] }}
         mainpitch={data.mainpitch || {}}
       />
-    )
+    );
   } else {
-    return (<div>Loading...</div>)
+    return <div>Loading...</div>;
   }
-}
+};
 
 IndexPagePreview.propTypes = {
   entry: PropTypes.shape({
-    getIn: PropTypes.func,
+    getIn: PropTypes.func
   }),
-  getAsset: PropTypes.func,
-}
+  getAsset: PropTypes.func
+};
 
-export default IndexPagePreview
+export default IndexPagePreview;
