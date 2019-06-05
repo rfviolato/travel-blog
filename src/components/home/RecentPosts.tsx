b928e9c4e31bcd45c4a383da2fd91bd96331279f
rfviolato <rfviolato@gmail.com>
2019-06-05 20:57:51 +0200

    layout(RecentPosts): Tweak design

--- src/components/home/RecentPosts.tsx
+++ src/components/home/RecentPosts.tsx
@@ -1,5 +1,6 @@
+/* tslint:disable:jsx-no-lambda */
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface IRecentPostsProps {
   posts: IRecentPostListItem[];
@@ -19,11 +20,16 @@ interface IPostItemBackgroundProps {
   src: string;
 }
 
+interface IOverlayProps {
+  isActive: boolean;
+}
+
 const PostItemContainer = styled.div`
-  background-color: red;
-  width: 225px;
-  height: 225px;
+  position: relative;
+  width: 250px;
+  height: 250px;
   border-radius: 5px;
+  cursor: pointer;
 `;
 
 const PostItemBackground = styled.div<IPostItemBackgroundProps>`
@@ -36,10 +42,50 @@ const PostItemBackground = styled.div<IPostItemBackgroundProps>`
   border-radius: 5px;
 `;
 
-const RecentPost: React.SFC<IRecentPost> = ({ imageSrc }) => {
+const Overlay = styled.div<IOverlayProps>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 15px;
+  color: #fff;
+  text-align: left;
+  z-index: 1;
+  opacity: ${({ isActive }) => (isActive ? '1' : '0')};
+  transition: opacity 330ms cubic-bezier(0.215, 0.61, 0.355, 1);
+`;
+
+const PostTitle = styled.div`
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const PostDescription = styled.div`
+  font-size: 12px;
+  line-height: 1.4em;
+  margin-top: 4px;
+`;
+
+const RecentPost: React.SFC<IRecentPost> = ({
+  imageSrc,
+  title,
+  description,
+}) => {
+  const [isActive, setActive] = useState(false);
+  console.log({ isActive });
+
   return (
-    <PostItemContainer>
+    <PostItemContainer
+      onMouseEnter={() => setActive(true)}
+      onMouseLeave={() => setActive(false)}
+    >
       <PostItemBackground src={imageSrc} />
+      <Overlay isActive={isActive}>
+        <PostTitle>{title}</PostTitle>
+        <PostDescription>{description}</PostDescription>
+      </Overlay>
     </PostItemContainer>
   );
 };
